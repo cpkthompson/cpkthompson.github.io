df5fd4b44ca074b96002d1883fb5fab77b1ef4aa
cpkthompson <thompsoncharles@live.com>
2017-07-19 23:43:32 +0000

    Debug fix

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = "j7fq^4tlmobn0*&a-6_jy=e0(na07t*r$4nkb5kumqpvdji336"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 
