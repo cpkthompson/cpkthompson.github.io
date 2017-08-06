1325a5b598cba7760f0cc1b69b2956eea3b6eb20
cpkthompson <thompsoncharles@live.com>
2017-08-06 19:20:09 +0000

    ALLOWED_HOST Fix

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ if socket.gethostname() == 'DESKTOP-78DDSQ9':
     ALLOWED_HOSTS = ['*']
 else:
     DEBUG = False
-    ALLOWED_HOSTS = ['.herokuapp.com']
+    ALLOWED_HOSTS = ['.herokuapp.com',]
 
 
 
