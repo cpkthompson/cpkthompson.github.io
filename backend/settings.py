d3dbbdae30a3fb253ff0428302e35375b4977083
Charles Thompson <thompsoncharles@live.com>
2017-08-15 21:09:08 +0000

    Added cpkthompson to ALLOWED_HOSTS

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ if socket.gethostname() == 'DESKTOP-78DDSQ9':
     ALLOWED_HOSTS = ['*']
 else:
     DEBUG = True
-    ALLOWED_HOSTS = ['.herokuapp.com']
+    ALLOWED_HOSTS = ['.herokuapp.com','cpkthompson.com']
 
 
 
